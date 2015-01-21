308d48b30d7638f4f1b861b8751f136c165ba17b
Sumin Byeon <suminb@gmail.com>
2015-01-21 23:21:23 +0900

    Proof of concept event handlers for checkpoint blocks

--- bnd/static/main.js
+++ bnd/static/main.js
@@ -3,3 +3,10 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+
+$(function() {
+  // Event handlers for checkpoint blocks
+  $('a.checkpoint-view').bind('click', function(evt) {
+    console.log($(evt.target).data('checkpoint-id'));
+  });
+});
