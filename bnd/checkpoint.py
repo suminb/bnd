a2e00f9fd9cbef410f2c78496b7271727e42a871
Sumin Byeon <suminb@gmail.com>
2015-02-15 15:46:37 +0900

    Checkpoint navigation

--- bnd/checkpoint.py
+++ bnd/checkpoint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask.ext.login import login_required, current_user
-from bnd.models import Checkpoint, Goal
+from bnd.models import Checkpoint, Team
 from bnd.forms import GoalForm
 from bnd.utils import handle_request_type
 
@@ -11,8 +11,14 @@ checkpoint_module = Blueprint(
 
 @checkpoint_module.route('/<int:checkpoint_id>')
 def view(checkpoint_id):
+    team_id, goal_id = map(request.args.get,
+                             ['team_id', 'goal_id'])
+
     checkpoint = Checkpoint.get_or_404(checkpoint_id)
+    team = Team.get_or_404(team_id)
+
     context = dict(
         checkpoint=checkpoint,
+        team=team,
     )
     return render_template('checkpoint/view.html', **context)
