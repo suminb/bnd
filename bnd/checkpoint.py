2a11d2bcfcb13c46e89045b0744cdc1e4f664329
Sumin Byeon <suminb@gmail.com>
2015-03-01 13:45:00 +0900

    Checkpoing evaluation

--- bnd/checkpoint.py
+++ bnd/checkpoint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask.ext.login import login_required, current_user
-from bnd.models import Checkpoint, Team, Goal
+from bnd.models import Checkpoint, Team, Goal, Evaluation
 from bnd.forms import GoalForm
 from bnd.utils import handle_request_type
+import re
 
 
 checkpoint_module = Blueprint(
@@ -25,3 +26,32 @@ def view(checkpoint_id):
         goals=goals,
     )
     return render_template('checkpoint/view.html', **context)
+
+
+@checkpoint_module.route('/<int:checkpoint_id>/evaluate', methods=['POST'])
+@login_required
+def evaluate(checkpoint_id):
+    team_id = request.args.get('team_id')
+
+    checkpoint = Checkpoint.get_or_404(checkpoint_id)
+    team = Team.get_or_404(team_id)
+
+    # TODO: Deal with multiple goals
+
+    for k, v in request.form.items():
+        m = re.match(r'goal-(?P<goal_id>\d+)', k)
+
+        if m is not None:
+            goal_id = m.group('goal_id')
+            Evaluation.create(
+                evaluation=v,
+                user=current_user,
+                checkpoint=checkpoint,
+                goal_id=goal_id,
+            )
+
+    context = dict(
+        checkpoint=checkpoint,
+        team=team,
+    )
+    return redirect(url_for('checkpoint.view', checkpoint_id=checkpoint.id, team_id=team.id))
