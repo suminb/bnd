f8ac38a974b6125e30d3624dd335402048c91441
Sumin Byeon <suminb@gmail.com>
2015-03-18 01:18:56 +0900

    Redirect www requests to non-www

--- bnd/controllers.py
+++ bnd/controllers.py
@@ -1,6 +1,20 @@
-
+try:
+    from urlparse import urlparse, urlunparse
+except ImportError:
+    from urllib.parse import urlparse, urlunparse
+from flask import request, redirect
 from bnd import create_app
 app = create_app(__name__)
+
+
+@app.before_request
+def redirect_www():
+    """Redirect www requests to non-www."""
+    urlparts = urlparse(request.url)
+    if urlparts.netloc.startswith('www.'):
+        urlparts_list = list(urlparts)
+        urlparts_list[1] = urlparts_list[1][len('www.'):]
+        return redirect(urlunparse(urlparts_list), code=301)
 
 
 if __name__ == '__main__':
